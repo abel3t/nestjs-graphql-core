db40bbdfc754ab1b2669d56ef300c8f76b88a5f8
Abel Tran <abeltran.develop@gmail.com>
2021-10-04 21:53:19 +0700

    remove source

--- src/shared/repositories/user.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-
-import { User } from '../entities/user.entity';
-import { BaseRepository } from './base.repository';
-
-@Injectable()
-@EntityRepository(User)
-export class UserRepository extends BaseRepository<User> {}
